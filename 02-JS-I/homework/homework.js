35c54c1e0f860090e8cfed71d518f07ea3b7795a
unknown <yamir.cuentas@gmail.com>
2022-07-26 00:15:55 -0500

    avance

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -73,11 +73,8 @@ function tienenMismaLongitud(str1, str2) {
   // Devuelve "true" si las dos strings tienen la misma longitud
   // De lo contrario, devuelve "false"
   // Tu código:
-  if (str1.length === str2.length) {
-    return true;
-  } else {
-    return false;
-  }
+  return str1.length === str2.length
+    
 }
 
 function menosQueNoventa(num) {
