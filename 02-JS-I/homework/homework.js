ceec333c14d331d2eec6a538c9bf6669a0a93e7f
unknown <yamir.cuentas@gmail.com>
2022-07-26 00:25:08 -0500

    por fin

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -234,7 +234,7 @@ function esVocal(letra){
   //que no se puede procesar el dato mediante el mensaje "Dato incorrecto".
   // Si no es vocal, tambien debe devolver "Dato incorrecto".
   //Escribe tu código aquí
-  if (letra>1){
+  if (letra.length > 1){
     return 'Dato incorrecto';
   } 
   if (letra === "a" || letra === "e" || letra === "i" || letra === "o" || letra === "u") {
