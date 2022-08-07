7dd6d63a7c0b5259d134689f932694c647321c12
unknown <yamir.cuentas@gmail.com>
2022-08-07 15:30:06 -0500

    avanzando

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -104,10 +104,10 @@ function estaEnRango(numero) {
   // De lo contrario, devuelve "false"
   // Tu código:
 if (numero < 50 && numero > 20){
-  return 'true';
+  return true;
 }
 else {
-  return 'false';
+  return false;
 }
 }
 
@@ -134,7 +134,7 @@ function fizzBuzz(numero) {
     return 'buzz';
   }
   else if (numero % 5 === 0 && numero % 3 === 0){
-    return 'fizzbuzz'
+    return 'fizzbuzz';
   }
   else {
     return numero;
@@ -148,8 +148,19 @@ function operadoresLogicos(num1, num2, num3) {
   //Si num3 es más grande que num1 y num2, aumentar su valor en 1 y retornar el nuevo valor.
   //0 no es ni positivo ni negativo. Si alguno de los argumentos es 0, retornar "Error".
   //Si no se cumplen ninguna de las condiciones anteriores, retornar false. 
-
+if (num1 > num2 && num1 > num3 && num>0){
+  return "Número 1 es mayor y positivo";
+} else if (num1 < 0 || num2 < 0 || num3 < 0){
+return "Hay negativos";
+} else if (num3 > num1 && num3 > num2){
+  return num3 + 1;
+} else if (num1 === 0 || num2 === 0 || num3 === 0){
+  return "Error";
+} else {
+  return false;
 }
+}
+
 
 function esPrimo(numero) {
   // Devuelve "true" si "numero" es primo
