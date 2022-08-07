b8ec24d779766894b387d1eb71b61a48bf6a07e1
unknown <yamir.cuentas@gmail.com>
2022-08-07 15:45:14 -0500

    avanzando

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -127,16 +127,16 @@ function fizzBuzz(numero) {
   // Si "numero" es divisible entre 5, devuelve "buzz"
   // Si "numero" es divisible entre 3 y 5 (ambos), devuelve "fizzbuzz"
   // De lo contrario, devuelve el numero
+  if (numero % 5 === 0 && numero % 3 === 0){
+    return 'fizzbuzz';
+  }
+  
   if (numero % 3 === 0){
     return 'fizz';
   }
   else if (numero % 5 === 0){
     return 'buzz';
-  }
-  else if (numero % 5 === 0 && numero % 3 === 0){
-    return 'fizzbuzz';
-  }
-  else {
+  } else {
     return numero;
   }
 }
@@ -148,17 +148,18 @@ function operadoresLogicos(num1, num2, num3) {
   //Si num3 es más grande que num1 y num2, aumentar su valor en 1 y retornar el nuevo valor.
   //0 no es ni positivo ni negativo. Si alguno de los argumentos es 0, retornar "Error".
   //Si no se cumplen ninguna de las condiciones anteriores, retornar false. 
-if (num1 > num2 && num1 > num3 && num1>0){
+
+  if (num1 < 0 || num2 < 0 || num3 < 0){
+  return "Hay negativos";
+  } else if (num1 > num2 && num1 > num3 && num1>0){
   return "Número 1 es mayor y positivo";
-} else if (num1 < 0 || num2 < 0 || num3 < 0){
-return "Hay negativos";
-} else if (num3 > num1 && num3 > num2){
+ } else if (num3 > num1 && num3 > num2){
   return num3 + 1;
-} else if (num1 === 0 || num2 === 0 || num3 === 0){
-  return "Error";
-} else {
+ } else if (num1 === 0 || num2 === 0 || num3 === 0){
+  return "Error"; 
+ } else {
   return false;
-}
+ }
 }
 
 
