f4be2e06ed58927458d49758f044b7888b829a31
unknown <yamir.cuentas@gmail.com>
2022-08-15 23:11:31 -0500

    otro poco

--- 05-JS-IV/homework/homework.js
+++ 05-JS-IV/homework/homework.js
@@ -46,19 +46,32 @@ function eliminarPropiedad (objeto, unaPropiedad) {
   // tip: tenes que usar bracket notation
   // Devuelve el objeto
   // Tu código:
+  delete objeto[unaPropiedad];
+
+  return objeto;
 }
 
 function nuevoUsuario (nombre, email, password) {
   // Crea un nuevo objeto con las propiedades coincidiendo con los argumentos que se pasan a la función
   // Devuelve el objeto
   // Tu código:
-
+  var obj = {
+    nombre: nombre,
+    email: email,
+    password: password,
+  };
+return obj;
 }
 
 function tieneEmail (usuario) {
   // Devuelve "true" si el usuario tiene un valor definido para la propiedad "email"
   // De lo contratio, devuelve "false"
   // Tu código:
+  if (usuario ['email']) {
+    return true;
+  } else {
+    return false;
+  }
 }
 
 function tienePropiedad (objeto, propiedad) {
@@ -66,6 +79,11 @@ function tienePropiedad (objeto, propiedad) {
   // "propiedad" es un string
   // De lo contrario, devuelve "false"
   // Tu código:
+  if (objeto [propiedad]) {
+    return true; 
+  } else {
+    return false;
+  }
 }
 
 function verificarPassword (usuario, password) {
@@ -73,6 +91,8 @@ function verificarPassword (usuario, password) {
   // Devuelve "true" si coinciden
   // De lo contrario, devuelve "false"
   // Tu código:
+  return usuario['password'] === 'password';
+  
 }
 
 function actualizarPassword (usuario, nuevaPassword) {
